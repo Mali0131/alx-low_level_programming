f13c4911a7fff65c5a36e1c105d98b0d22688bd8
Muhammad Ali <mehammadali999@gmail.com>
2022-07-01 07:32:47 -0700

    My

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,20 +1,12 @@
 #include <stdio.h>
-
 /**
-
  * main - Entry point
  *
- *  * Return: Always 0 (sucess)
- *
- *   
+ * Return: Always 0 (success)
  */
-
- int main(void)
-
+int main(void)
 {
-
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"programming is like building a multilingual  puzzle");
 
 	return (0);
-
 }
