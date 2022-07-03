58358a57c5930cabbf805daa8c0565800f498615
Muhammad Ali <mehammadali999@gmail.com>
2022-07-03 15:59:41 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,53 +1,20 @@
 #include <stdio.h>
-
-
-
 /**
+ * main - output all single digit number of base 10
  *
- *  *main -> assign a random number to the variable n each time it is executed
- *
- *   *and print the last digit of the number stored in the variable n
- *
- *    *Return: Always 0 (Success)
- *
- *     */
-
+ * Return: 0 if success
+ */
 int main(void)
-
 {
+	int var;
+	char var1;
 
-	char ch;
+	for (var = 0; var < 10; var++)
+		putchar((var % 10) + '0');
 
-	int n;
-
-
-
-	for (n = 48; n <= 57; n++)
-
-	{
-
-
-
-		putchar(n);
-
-	}
-
-	for (ch = 'a'; ch <= 'f'; ch++)
-
-	{
-
-
-
-		putchar(ch);
-
-	}
-
-
-
-	putchar(10);
-
-
-
+	for (var1 = 'a'; var1 <= 'f'; var1++)
+		putchar(var1);
+	putchar('\n');
+	
 	return (0);
-
 }
