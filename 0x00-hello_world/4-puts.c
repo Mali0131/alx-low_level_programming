86ac677757fd42fda1a36b6204d50d125874673c
Muhammad Ali <mehammadali999@gmail.com>
2022-07-03 02:37:00 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,9 @@
  */
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle"); 
 
-	return (0);
+	puts("\"programming is like building a multilingual puzzle");	
+
+	        return (0);
+
 }
