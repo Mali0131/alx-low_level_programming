afff97d4f19e0b1ab6f34b554bbec75dc76986b7
Muhammad Ali <mehammadali999@gmail.com>
2022-07-03 11:29:26 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,35 +1,17 @@
 #include <stdio.h>
-
-
-
+#include <stdlib.h>
 /**
+ * main - output all single digit number of base 10
  *
- *  *main -> assign a random number to the variable n each time it is executed
- *
- *   *and print the last digit of the number stored in the variable n
- *
- *    *Return: Always 0 (Success)
- *
- *     */
-
+ * Return: 0 if success
+ */
 int main(void)
-
 {
+	int var;
 
-	int n;
-
-
-
-	for (n = 0; n < 10; n++)
-
-	{
-
-		printf("%d", n);
-
-	}
-
+	for (var = 0; var <= 10; var++)
+		printf("%d", var);
 	printf("\n");
 
 	return (0);
-
 }
