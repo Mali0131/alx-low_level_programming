d590e2e807388b3ef6cb4fb2b2ef5e70c8a364c0
Muhammad Ali <mehammadali999@gmail.com>
2022-07-03 09:15:19 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,37 +1,18 @@
 #include <stdio.h>
-
-
-
+#include <stdlib.h>
 /**
+ * main - print all letters of the alphabeth in lower case
  *
- *  *main -> assign a random number to the variable n each time it is executed
- *
- *   *and print the last digit of the number stored in the variable n
- *
- *    *Return: Always 0 (Success)
- *
- *     */
-
+ * Return: 0 if success
+ */
 int main(void)
-
 {
+	char var;
 
-		int ch;
-
-
-
-			for (ch = 97; ch <= 122; ch++)
-
-					{
-
-								putchar(ch);
-
-									}
-
-				putchar(10); /* this is an ascii code for new line*/
-
-
-
-					return (0);
-
+	for (var = 'a''; var <= 'z'; var++)
+	{
+		putchar(var);
+	}
+	putchar('\n');
+	return (0);
 }
