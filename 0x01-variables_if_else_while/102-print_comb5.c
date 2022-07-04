1aeca9775e62fce82ece7e795108b6035ba76df5
Muhammad Ali <mehammadali999@gmail.com>
2022-07-04 01:04:22 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - output all single digit number od base 10
+ * main - output all single digit number of base 10
  *
  * Return: 0 if success
  */
@@ -9,13 +9,14 @@ int main(void)
 	int var;
 
 	for (var = 0; var < 99; var++)
-        {
+	{
 		int ii;
-		
+
 		for (ii = var + 1; ii < 100; ii++)
 		{
 			putchar((var / 10) + '0');
 			putchar((var % 10) + '0');
+			putchar(' ');
 			putchar((ii / 10) + '0');
 			putchar((ii % 10) + '0');
 
