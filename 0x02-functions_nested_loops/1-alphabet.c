0ca04cd9d01d906774e6b47cfd575c6ce9b80514
Muhammad Ali <mehammadali999@gmail.com>
2022-07-06 22:56:02 -0700

    s

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,28 @@
-#include "main.h"
+nclude "main.h"
+
 /**
+ *
  * print_alphabet - prints alphabet in lower case
  * i
  * created : Miminwa
- * cc: july, 2022
+ *
+ * cc: 5th july, 2022
+ *
+ *
  *
  * Return: void
  */
-void print_alphabet(void)
-{
-	int ii;
 
-	for (ii = 'a'; ii <= 'z'; ii++)
-		_putchar(ii);
-	_putchar('\n');
-}		
+void print_alphabet(void)
+
+{
+
+		int ii;
+
+			for (ii = 'a'; ii <= 'z'; ii++)
+
+						_putchar(ii);
+
+				_putchar('\n');
+
+}
