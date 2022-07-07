b039a081ce24520e42d368140b0380a2838e9b1f
Muhammad Ali <mehammadali999@gmail.com>
2022-07-07 00:00:54 -0700

    l

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _isalpha - checks for alphabetic character.
+ *
+ * Return: 1 if c is a letter, 0 otherwise
+ */
+int _isalpha(int c)
+{
+	int lower = (c >= 'a' && c <= 'z')
+	int upper = (c >= 'A' && c <= 'Z')
+
+	if (lower || upper)
+		return (1);
+	else
+	       return (0);
+}	
