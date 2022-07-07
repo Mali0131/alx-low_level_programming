dc46f4061380d5ed4634cf1fd4fe91fb9aba5b5f
Muhammad Ali <mehammadali999@gmail.com>
2022-07-07 01:11:04 -0700

    k

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,24 +1,38 @@
 #include "main.h"
 /**
- * jack_bauer - prints every minute of the day of
- * jack Bauer, starting from 00:00 to 23:59.
+ * times_table - print the 9 times table, starting with 0
  */
-void jack_bauer(void)
+void times_table(void)
 {
-	int hour;
-	int minute;
+	int i,j,k;
 
-	for (hour = 0; hour <= 23; hour++)
+	for (i = 0; i < 10; i++)
 	{
-		for (minute = 0; minute <= 59; minute++)
+		for (j = 0; j < 10; j++)
 		{
-			_putchar((hour / 10) + '0');
-			_putchar((hour % 10) + '0');
-			_putchar(':');
-			_putchar((minute / 10) + '0');
-			_putchar((minute % 10) + '0');
-			_putchar('\n');
+			k = j * i;
+			if (j == 0)
+			{
+				_putchar(k + '0');
+			}
+
+			if (k < 10 && j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(k + '0');
+			}
+			else if (k >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
+			}
 		}
+			
+			_putchar('\n');
 	}
 }
-	       	       
+
