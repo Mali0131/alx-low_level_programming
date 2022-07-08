d32326511206dfd48c516b8543d19888be7556a0
Muhammad Ali <mehammadali999@gmail.com>
2022-07-08 08:03:03 -0700

    f

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -12,21 +12,16 @@ void print_line(int n)
 {
 
 		int x;
-
-			if (n <= 0)
-
-						_putchar('\n');
-
-				else
-
-						{
-
-									for (x = 0; x < n; x++)
-
-													_putchar(95);
-
-											_putchar('\n');
-
-												}
-
+		
+		if (n <= 0)
+			
+			_putchar('\n');
+		else
+		{
+			for (x = 0; x < n; x++)
+				
+				_putchar(95);
+				_putchar('\n');
+		}
+		
 }
