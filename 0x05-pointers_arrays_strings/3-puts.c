4269e3f55dbcc51b03af7639f9167c100838450f
Muhammad Ali <mehammadali999@gmail.com>
2022-07-12 14:49:52 -0700

    f

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+
+
+/**
+ * _puts - prints a string
+ *
+ * @str: string to be printer
+ *
+ * Created by : Sanctus-Peter
+ *
+ * cc: 12th july, 2022.
+ */
+
+
+
+void _puts(char *str)
+
+{
+
+		int i = 0;
+
+
+
+		while (str[i] != '\0')
+
+		{
+			_putchar(str[i]);
+			
+			i++;
+		}
+		
+		_putchar('\n');
+
+}
