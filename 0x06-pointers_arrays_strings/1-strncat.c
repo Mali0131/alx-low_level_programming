08ae415a9f7f57e28af46eca1ed3735749071b5b
Muhammad Ali <mehammadali999@gmail.com>
2022-07-14 15:06:29 -0700

    k

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -21,8 +21,7 @@ char *_strncat(char *dest, char *src, int n)
 		;
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
-
-			dest[dest_len + i] = src[i];
+		dest[dest_len + i] = src[i];
 
 	/*should end with a end of string char*/
 	dest[dest_len + i] = '\0';
