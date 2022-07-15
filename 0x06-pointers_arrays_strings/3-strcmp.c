0f554c703dfeda41a45f01660ff6e5878401d84d
Muhammad Ali <mehammadali999@gmail.com>
2022-07-15 09:49:12 -0700

    k

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,8 +1,7 @@
-nclude "main.h"
+#include "main.h"
 
 /**
- * _strcmp - compares two strings
- *
+ * _strcmp - compares two string.
  * @s1: first string
  *
  * @s2: second string
@@ -21,6 +20,7 @@ int _strcmp(char *s1, char *s2)
 		{
 			return (s1[i] - s2[i]);
 		}
+
 	}
 
 	return (0);
