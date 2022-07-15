544bc34f4730994886a907b925b02c273aab6070
Muhammad Ali <mehammadali999@gmail.com>
2022-07-15 09:19:37 -0700

    k

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase.
+ * @s: the string
+ *
+ * Return: uppercase
+ */
+
+char *string_toupper(char *s)
+
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] -= 32;
+		i++;
+	}
+	return (s);
+}
