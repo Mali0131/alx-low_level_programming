0adb65e8829dfe9c9a6b1ad4de9a37d0530a68b7
Muhammad Ali <mehammadali999@gmail.com>
2022-07-15 08:35:34 -0700

    l

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,9 @@
+#ifndef MAIN_H
+
+#define MAIN_H
+
+
+
 int _putchar(char c);
 
 char *_strcat(char *dest, char *src);
@@ -22,4 +28,8 @@ void print_number(int n);
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 
-void print_buffer(char *
+void print_buffer(char *b, int size);
+
+
+
+#endif
