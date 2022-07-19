5980a1f28f7847ca81a01815d0b103ad01c9d849
Muhammad Ali <mehammadali999@gmail.com>
2022-07-19 09:37:46 -0700

    l

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -8,7 +8,6 @@
  * _strpbrk - searches a string for any of a set of bytes.
  * @s: the string
  * @accept: a set of bytes
- * 
  * Return: a pointer to the byte in s that matches one of the bytes
  *
  * in accept, or NULL if no such byte is found
