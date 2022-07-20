f26107ad961b27c71633d2b6677ce7db51c09466
Muhammad Ali <mehammadali999@gmail.com>
2022-07-20 16:49:15 -0700

    l

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -7,12 +7,16 @@ int get_sqrt(int n, int possible_root);
 
 
 /**
- * _sqrt_recursion - returns the natural square root of a number.
  *
- * @n: the number
+ *  * _sqrt_recursion - returns the natural square root of a number.
  *
- * Return: returns the natural square root of a number.
- /
+ *   * @n: the number
+ *
+ *    *
+ *
+ *     * Return: returns the natural square root of a number.
+ *
+ *      */
 
 int _sqrt_recursion(int n)
 
@@ -21,13 +25,20 @@ int _sqrt_recursion(int n)
 }
 
 /**
- * get_sqrt - tries to get the square root of n by continously
- * comparing the square of 'possible_root' to n
- * @n: the number
- * @possible_root: a possible root of n
  *
- * Return: square root of n, -1 otherwise
- */
+ *  * get_sqrt - tries to get the square root of n by continously
+ *
+ *   * comparing the square of 'possible_root' to n
+ *
+ *    * @n: the number
+ *
+ *     * @possible_root: a possible root of n
+ *
+ *      *
+ *
+ *       * Return: square root of n, -1 otherwise
+ *
+ *        */
 
 int get_sqrt(int n, int possible_root)
 
@@ -37,7 +48,4 @@ int get_sqrt(int n, int possible_root)
 	if (square == n)
 		return (possible_root);
 	if (square < n)
-		return (get_sqrt(n, ++possible_root));
-
-	return (-1);
-}
+		
