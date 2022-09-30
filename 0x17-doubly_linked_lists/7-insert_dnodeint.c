ccf991c64374847ef7b7855a99d4ec1641ef7cf4
Muhammad Ali <mehammadali999@gmail.com>
2022-09-30 15:48:48 -0700

    l

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,43 +1,41 @@
 #include "lists.h"
 
 /**
-* insert_dnodeint_at_index - function that inserts
-* a new node at a given position.
-* @h: double pointer to a part of the node
-* @idx: index to add the node
-* @n: element of the node to add
-* Return:address of the new node
-*/
+ * insert_dnodeint_at_index - Inserts a new node in a dlistint_t
+ *                            list at a given position.
+ * @h: A pointer to the head of the dlistint_t list.
+ * @idx: The position to insert the new node.
+ * @n: The integer for the new node to contain.
+ *
+ * Return: If the function fails - NULL.
+ *         Otherwise - the address of the new node.
+ */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-dlistint_t *head = *h;
-dlistint_t *a, *b;
-unsigned int pos = 0;
+	dlistint_t *tmp = *h, *new;
 
-a = malloc(sizeof(dlistint_t));
-if (a == NULL)
-return (NULL);
-if (head != NULL)
-{
-while (head->prev != NULL)
-head = head->prev;
-}
+	if (idx == 0)
+		return (add_dnodeint(h, n));
 
-while (head != NULL && pos != (idx - 1))
-{
-head = head->next;
-pos++;
-}
-if (pos == (idx - 1))
-{
-b = head->next;
-head->next = a;
-a->prev = head;
-a->n = n;
-a->next = b;
-b->prev = a;
-return (a);
-}
+	for (; idx != 1; idx--)
+	{
+		tmp = tmp->next;
+		if (tmp == NULL)
+			return (NULL);
+	}
 
-return (NULL);
+	if (tmp->next == NULL)
+		return (add_dnodeint_end(h, n));
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->prev = tmp;
+	new->next = tmp->next;
+	tmp->next->prev = new;
+	tmp->next = new;
+
+	return (new);
 }
--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,41 +1,42 @@
 #include "lists.h"
 
 /**
-* delete_dnodeint_at_index - function that deletes
-* the node at index index of a dlistint_t linked list.
-* @head: double pointer to a part of the node
-* @index: index to add the node to delete
-* Return: 1 (successful) or -1 !(sucessful)
-*/
-
+ * delete_dnodeint_at_index - Deletes a node from a dlistint_t
+ *                            at a given index.
+ * @head: A pointer to the head of the dlistint_t.
+ * @index: The index of the node to delete.
+ *
+ * Return: Upon success - 1.
+ *         Otherwise - -1.
+ */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-dlistint_t *h = *head;
-dlistint_t *a;
-unsigned int pos = 0;
+	dlistint_t *tmp = *head;
 
-if (h != NULL)
-{
-while (h->prev != NULL)
-h = h->prev;
-}
-while (h != NULL && pos != index)
-{
-h = h->next;
-pos++;
-}
-if (pos == index)
-{
-a = h->next;
-if (h->prev != NULL)
-{
-h->prev->next = a;
-a->prev = h->prev;
-}
-else
-a->prev = NULL;
-free(h);
-return (1);
-}
-return (-1);
+	if (*head == NULL)
+		return (-1);
+
+	for (; index != 0; index--)
+	{
+		if (tmp == NULL)
+			return (-1);
+		tmp = tmp->next;
+	}
+
+	if (tmp == *head)
+	{
+		*head = tmp->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
+	}
+
+	else
+	{
+		tmp->prev->next = tmp->next;
+		if (tmp->next != NULL)
+			tmp->next->prev = tmp->prev;
+	}
+
+	free(tmp);
+	return (1);
 }
